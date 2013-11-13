02fb72adda6804cf9afb6211c2c93b5a0c83c467
praneeth <praneeth@praneeths-mbp.gateway.2wire.net>
2013-11-12 21:23:37 -0800

    Config

--- Client/src/sy/video/model/Config.java
+++ Client/src/sy/video/model/Config.java
@@ -1,7 +1,8 @@
 package sy.video.model;
 
 public class Config {
-	public static final Boolean CLOUD_DEPLOY = Boolean.FALSE;
+	public static final Boolean CLOUD_MYSQL_DEPLOY = Boolean.FALSE;
+	public static final Boolean CLOUD_MONGO_DEPLOY = Boolean.FALSE;
 	
 	// Local End Ppoint
 	public static final String ENDPOINT_USER = "http://localhost:8080/VideoLibraryServer/services/UserModel";
@@ -10,27 +11,38 @@ public class Config {
 
 	
 	//Cloud-MySQL End Point.
-	/*
-	public static final String ENDPOINT_USER_CLOUD = "http://videoserver.aws.af.cm/services/UserModel";
-	public static final String ENDPOINT_MOVIE_CLOUD = "http://videoserver.aws.af.cm/services/VideoModel";
-	public static final String ENDPOINT_RENTAL_CLOUD = "http://videoserver.aws.af.cm/services/RentalModel";
-	*/
+	public static final String ENDPOINT_MYSQL_USER_CLOUD = "http://videoserver.aws.af.cm/services/UserModel";
+	public static final String ENDPOINT_MYSQL_MOVIE_CLOUD = "http://videoserver.aws.af.cm/services/VideoModel";
+	public static final String ENDPOINT_MYSQL_RENTAL_CLOUD = "http://videoserver.aws.af.cm/services/RentalModel";
+	
 	
 	//Cloud-Mongo End Point.
-	public static final String ENDPOINT_USER_CLOUD = "http://videoserver-mongo.aws.af.cm/services/UserModel";
-	public static final String ENDPOINT_MOVIE_CLOUD = "http://videoserver-mongo.aws.af.cm/services/VideoModel";
-	public static final String ENDPOINT_RENTAL_CLOUD = "http://videoserver-mongo.aws.af.cm/services/RentalModel";
+	public static final String ENDPOINT_MONGO_USER_CLOUD = "http://videoserver-mongo.aws.af.cm/services/UserModel";
+	public static final String ENDPOINT_MONGO_MOVIE_CLOUD = "http://videoserver-mongo.aws.af.cm/services/VideoModel";
+	public static final String ENDPOINT_MONGO_RENTAL_CLOUD = "http://videoserver-mongo.aws.af.cm/services/RentalModel";
 	
 	
 	public static String getUserModelEndPoint() {
-		return CLOUD_DEPLOY ? ENDPOINT_USER_CLOUD : ENDPOINT_USER;
+		if ( CLOUD_MYSQL_DEPLOY) 
+			return ENDPOINT_MYSQL_USER_CLOUD;
+		if ( CLOUD_MONGO_DEPLOY)
+			return ENDPOINT_MONGO_USER_CLOUD;
+		return ENDPOINT_USER;
 	}
 	
 	public static String getMovieModelEndPoint() {
-		return CLOUD_DEPLOY ? ENDPOINT_MOVIE_CLOUD : ENDPOINT_MOVIE;
+		if ( CLOUD_MYSQL_DEPLOY) 
+			return ENDPOINT_MYSQL_MOVIE_CLOUD;
+		if ( CLOUD_MONGO_DEPLOY)
+			return ENDPOINT_MONGO_MOVIE_CLOUD;
+		return ENDPOINT_MOVIE;
 	}
 	
 	public static String getRentalModelEndPoint() {
-		return CLOUD_DEPLOY ? ENDPOINT_RENTAL_CLOUD : ENDPOINT_RENTAL;
+		if ( CLOUD_MYSQL_DEPLOY) 
+			return ENDPOINT_MYSQL_RENTAL_CLOUD;
+		if ( CLOUD_MONGO_DEPLOY)
+			return ENDPOINT_MONGO_RENTAL_CLOUD;
+		return ENDPOINT_RENTAL;
 	}
 }
